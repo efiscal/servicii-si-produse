a5b13da2ce576d9d551947d914fa2b1de813b3f0
Octavian T. <octavian@tabuci.org>
2025-02-14 17:11:01 +0200

    SPRE CERTIFICARE v1.0

--- bin/checksum.sh
+++ bin/checksum.sh
@@ -10,10 +10,14 @@ else
     exit 1
 fi
 
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes &>/dev/null
+if [ $? -ne 0 ]; then
+    echo "Failed to run multiarch/qemu-user-static"
+    exit 1
+fi
 
-amd64_output=$(docker run -it --rm --platform linux/amd64 ${REGISTRY_DOMAIN}/${CORE_VERSION} cat /ecc/ecc.sha256)
+amd64_output=$(docker run -it --rm --platform linux/amd64 ${REGISTRY_DOMAIN}/${CORE_VERSION} cat /ecc/ecc.sha256 2>/dev/null)
 echo "LINUX/AMD64: $amd64_output"
 
-arm64_output=$(docker run -it --rm --platform linux/arm64 ${REGISTRY_DOMAIN}/${CORE_VERSION} cat /ecc/ecc.sha256)
+arm64_output=$(docker run -it --rm --platform linux/arm64 ${REGISTRY_DOMAIN}/${CORE_VERSION} cat /ecc/ecc.sha256 2>/dev/null)
 echo "LINUX/ARM64: $arm64_output"
